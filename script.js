9ba8237a80cde3a40fa5843b87d640b3ba305bcf
vraikar-git <vijay7.raikar@gmail.com>
2022-01-01 20:18:48 +0530

    major changes

--- script.js
+++ script.js
@@ -1,11 +1,55 @@
 
 const grid = document.querySelector('.grid');
-const reset = document.querySelector('#reset')
+const btns = document.querySelectorAll('button');
+const slider = document.querySelector('#slider');
+const gridSizeDiv = document.querySelector(".grid-size")
+let mode = 0;  // 1 : random color, 0 : balck and white 2: shadding
 let number = 5;
 
-reset.addEventListener('click',doReset);
+btns.forEach((btn) => btn.addEventListener('click',controls));
 grid.addEventListener('mouseover',changeColor);
+slider.addEventListener('input', generateGridDivsOnchange);
+
 generateGridDivs(number)
+
+
+function controls(){
+
+    switch(this.id){
+        case 'reset':
+            doReset();
+            break;
+        case 'rainbow':
+            mode = 1;
+            break;
+        case 'shadding':
+            mode = 2;
+            break;
+        default :
+            mode = 0;
+    }
+}
+
+function getColor(modeN, event){
+
+    switch(modeN){
+        case 1: return getRandomColor();
+        case 2: return getShadeColor(event);
+        default: return  "rgb(0,0,0)";
+    }
+}
+
+function getShadeColor(event){
+    if(event.target.style.backgroundColor === "")
+        return "rgb(255,255,255)";
+    let divColors = event.target.style.backgroundColor.match(/[\d]+/g);
+    console.log("getShadeCOlor : " + event.target.style.backgroundColor);
+    let newColors = divColors.map(item => Number.parseInt(item) - 10);
+    return "rgb(" + `${newColors[0]},${newColors[1]},${newColors[2]}` + ")";
+    
+    
+}
+
 
 function getRandomColor(){
     const letters = '0123456789ABCDEF';
@@ -17,29 +61,33 @@ function getRandomColor(){
 }
 
 function doReset(){
-    grid.textContent = '';
-    number = Number.parseInt(prompt('Enter Number',10));
-    if( number > 100){
-        alert("Number too big to render");
-        return;
-    }
-    generateGridDivs(number);
+    
+    generateGridDivs(5);
+    
 
 };
 
 function changeColor(e){
-    // console.log(e.target);
-    e.target.setAttribute("style",`background-color:${getRandomColor()}`);
+    e.target.setAttribute("style",`background-color:${getColor(mode,e)}`);
 }
 
-function generateGridDivs(number){
-    grid.setAttribute("style",`grid-template-columns: repeat(${number}, 1fr);`)
-    for(i = 1; i <= number*number ; i++){
+function generateGridDivs(gridSize){
+    gridSizeDiv.textContent = `${gridSize} x ${gridSize}`;
+    grid.textContent = '';
+    console.log("generateGridDiv : ", gridSize);
+    grid.setAttribute("style",`grid-template-columns: repeat(${gridSize}, 1fr);`)
+    for(i = 1; i <= gridSize*gridSize ; i++){
         const div = document.createElement('div');
         div.setAttribute('class', 'square');
         grid.appendChild(div);
         
     };
+};
+
+function generateGridDivsOnchange(event){
+    console.log("Onchange : : " + event.target.value);
+    //let txt = 
+    generateGridDivs(event.target.value);
 };
 
 
